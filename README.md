466b67aa40f4440a042e15b4baaa7ac76080adfc
Sara Kauvo <sara.kau@hotmail.com>
2019-02-03 02:59:22 +0200

    yritys

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Full-Stack-websovelluskehitys-2019-osa3
+
+https://obscure-hamlet-65476.herokuapp.com/api/persons
---
+++ build/precache-manifest.c6520b0aa8165858feb07d739acac4ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "545515dea99c8bd90ca5",
+    "url": "/static/css/main.b2a136f5.chunk.css"
+  },
+  {
+    "revision": "545515dea99c8bd90ca5",
+    "url": "/static/js/main.545515de.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63f0b042284626fdf56a96c44a329c76",
+    "url": "/index.html"
+  }
+];
